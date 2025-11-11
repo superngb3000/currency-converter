4a3bf8c4819c9102feef2f09ea896ef91c375f85
superngb3000 <foxsfoxs765@yandex.ru>
2025-11-12 01:13:20 +0300

    init

---
+++ currency-converter-back/src/main/java/org/superngb/currencyconverterback/CurrencyConverterBackApplication.java
@@ -0,0 +1,13 @@
+package org.superngb.currencyconverterback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConverterBackApplication.class, args);
+	}
+
+}
---
+++ currency-converter-back/src/test/java/org/superngb/currencyconverterback/CurrencyConverterBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.superngb.currencyconverterback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
